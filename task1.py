2b3c27571351fdde2441ffb1bec4d9ef3f885609
AKGupta-art <anmolgupta82726@gmail.com>
2025-04-28 22:10:22 +0530

    add code for task1 module1

---
+++ task1.py
@@ -0,0 +1,7 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+print("Addition_result: ", num1 + num2)
+print("Subtraction_result: ", num1 - num2)
+print("Multiplication_result: ",num1 * num2)
+print("Division_result: ",num1 / num2)
