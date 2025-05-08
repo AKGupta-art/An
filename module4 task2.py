7bf7bc694d5a80862b5afdafbaa0f3ea1aaf652e
AKGupta-art <anmolgupta82726@gmail.com>
2025-05-08 07:23:07 +0530

    Add files via upload

---
+++ module4 task2.py
@@ -0,0 +1,14 @@
+import math
+
+number_str = input("Enter a number: ")
+number = float(number_str)
+
+sqrt_result = math.sqrt(number)
+
+log_result = math.log(number)
+
+sin_result = math.sin(number)
+
+print("Square root:", sqrt_result)
+print("Natural logarithm:", log_result)
+print("Sine:", sin_result)
